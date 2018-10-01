0b00cab896d5c4d56918caa11441ec29b4d96007
Gustavo Guichard <gustavoguichard@gmail.com>
2018-10-01 11:20:06 -0300

    Add headers

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,14 @@
+import Head from 'next/head'
 import Menu from './Menu'
 import 'styles/main.scss'
 
 const Layout = props => (
   <div className="app-wrapper">
+    <Head>
+      <title>Vida Natural | Cosmética Consciente</title>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
     <Menu />
     {props.children}
   </div>
