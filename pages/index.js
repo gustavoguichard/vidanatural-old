0a2eb0ffdafc29586674d25e00ea3785c295814f
Gustavo Guichard <gustavoguichard@gmail.com>
2018-11-04 01:57:47 -0200

    Contact

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import About from 'components/About'
+import Contact from 'components/Contact'
 import Home from 'components/Home'
 import Layout from 'components/Layout'
 import People from 'components/People'
 
-export default () =>
+export default () => (
   <Layout>
     <Home />
     <About />
     <People />
+    <Contact />
   </Layout>
+)
