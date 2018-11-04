92f0a8f4f485f6299a036afa507b9a9ddf3ebcf1
Gustavo Guichard <gustavoguichard@gmail.com>
2018-11-04 15:16:49 -0200

    About in Home Page

--- components/About.js
+++ components/About.js
@@ -1,49 +1,45 @@
 import { memo } from 'react'
-import { Columns, Content } from 'react-bulma-components'
+import ReactMarkdown from 'react-markdown'
+import { Button, Columns, Content } from 'react-bulma-components'
 import Logo from 'components/Logo'
 import PageBreadCrumb from 'components/menu/PageBreadCrumb'
 import ImageContainer from 'components/ImageContainer'
 import { useMedia } from 'utils/hooks'
 
+import content from 'content/about'
 import 'styles/about.scss'
 
 export default memo(() => {
   const isDesktop = useMedia('desktop')
   return (
-  <Columns id="sobre" className="about-section" gapless>
-    <Columns.Column
-      key="design"
-      className="about-header"
-      style={{ display: 'flex' }}
-    >
-      <ImageContainer
-        src="/static/about-bg.jpg"
-        key="about-bg"
-        className="about-bg"
-        contentClass="about-bg-content"
-        fixed
+    <Columns id="sobre" className="about-section" gapless>
+      <Columns.Column
+        key="design"
+        className="about-header"
+        style={{ display: 'flex' }}
       >
-        <Logo clickable key="logo" />
-        {isDesktop && <PageBreadCrumb key="breadcrumb" title="sobre" />}
-      </ImageContainer>
-    </Columns.Column>
-    <Columns.Column className="content-wrapper">
-      <Content key="content" className="black-content">
-        <h2 className="title is-4">Sobre</h2>
-        <p>
-          Consciêcia talvez seja a palavra mais importante no nosso
-          vocabulário. E, com o tempo, você vai perceber que ela está
-          presente nas nossas iniciativas e, também, na forma como nos
-          relacionamos com as pessoas, produzimos e distribuímos os nossos
-          cosméticos.
-        </p>
-        <p>
-          Em breve, você conhecerá muito sobre nós, inclusive o nosso
-          processo de produção. Tudo para motivar você a dizer{' '}
-          <strong>eu uso</strong> cosmética consciente e nos auxiliar a
-          completar a grafia do nosso posicionamento.
-        </p>
-      </Content>
-    </Columns.Column>
-  </Columns>
-)})
+        <ImageContainer
+          src="/static/about-bg.jpg"
+          key="about-bg"
+          className="about-bg"
+          contentClass="about-bg-content"
+          fixed
+        >
+          <Logo clickable key="logo" />
+          {isDesktop && <PageBreadCrumb key="breadcrumb" title="sobre" />}
+        </ImageContainer>
+      </Columns.Column>
+      <Columns.Column className="content-wrapper">
+        <Content key="content" className="black-content">
+          <h2 className="title is-4">Sobre a VN</h2>
+          <ReactMarkdown className="md-content" source={content} />
+          <p>
+            <Button color="light" className="is-large" rounded outlined>
+              Quero saber mais
+            </Button>
+          </p>
+        </Content>
+      </Columns.Column>
+    </Columns>
+  )
+})
---
+++ content/about.js
@@ -0,0 +1,14 @@
+export default `
+Acreditamos que cada um de nós, através de nossas pequenas e
+constantes **ações**, tem o poder de construir grandes coisas.
+
+Somos todos capazes de **transformar**! De onde estamos, com o que temos
+e a partir do que já somos!
+
+Acreditamos que cada pequena **atitude** tem o poder de gerar grandes
+transformações.
+
+> A responsabilidade das nossas escolhas vai muito além de nós.
+>
+> Esteja cada vez mais integrado e consciente dessa rede.
+`
