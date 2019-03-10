145d37db11b5b196209adc4b2d853a0a50164d25
Gustavo Guichard <gustavoguichard@gmail.com>
2019-03-10 17:48:59 -0300

    Add theme-light option

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,18 @@
 import { memo } from 'react'
+import classnames from 'classnames'
 import Menu from './Menu'
 import Footer from './Footer'
 import 'styles/main.scss'
 
-const Layout = ({ children }) => (
-  <div className="app-wrapper">
-    <Menu />
-    {children}
-    <Footer />
-  </div>
-)
+const Layout = ({ children, light = false }) => {
+  const classes = classnames('app-wrapper', { 'theme-light': light })
+  return (
+    <div className={classes}>
+      <Menu />
+      {children}
+      <Footer />
+    </div>
+  )
+}
 
 export default memo(Layout)
--- components/menu/SocialMenu.js
+++ components/menu/SocialMenu.js
@@ -1,13 +1,30 @@
 import { memo } from 'react'
 import { TiSocialFacebook, TiSocialInstagram } from 'react-icons/ti'
 
-export default memo(() =>
-  <aside className="social-icons">
-    <a href="https://facebook.com/vidanatural.eco/" title="Facebook" target="blank">
+export default memo(() => (
+  <aside
+    className="social-icons"
+    css={{
+      position: 'fixed',
+      top: '1.5rem',
+      left: '2rem',
+      zIndex: 1000,
+    }}
+  >
+    <a
+      css={{ marginRight: '0.6em' }}
+      href="https://facebook.com/vidanatural.eco/"
+      title="Facebook"
+      target="blank"
+    >
       <TiSocialFacebook size="1.8em" />
     </a>
-    <a href="https://www.instagram.com/vidanatural.eco/" title="Instagram" target="blank">
+    <a
+      href="https://www.instagram.com/vidanatural.eco/"
+      title="Instagram"
+      target="blank"
+    >
       <TiSocialInstagram size="1.8em" />
     </a>
   </aside>
-)
+))
