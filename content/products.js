8f2f5fe61468cb42187f1ab03dd34cd2d228bfbb
Gustavo Guichard <gustavoguichard@gmail.com>
2019-03-25 20:16:34 -0300

    Add product benefits

--- content/products.js
+++ content/products.js
@@ -4,6 +4,14 @@ export default [
     path: 'oleo-hidratante',
     ingredients:
       'HELIANTHUS ANNUUS SEED (Óleo de Girassol 100% vegetal e prensado a frio), LAVANDULA ANGUSTIFOLIA OIL (Óleo Essencial de Lavanda), CYMBOPOGON MARTINI (Óleo Essencial de Palmarosa).',
+    benefits: [
+      'Além de hidratar, nutre a pele',
+      'Óleos minerais e seus derivados não penetram na nossa pele, eles apenas criam uma película sobre ela',
+      'Já os óleos vegetais e prensados a frio penetram na nossa pele, hidratam e nutrem.Permitem a respiração cutânea e a assimilação da luz solar',
+      'Extração a frio, preserva elementos naturais dos óleos como vitaminas, fosfolipídios, antioxidantes, antiinflamatórios, entre outros',
+      'Essas substâncias favorecem a bioatividade da pele, hidratando o tecido, combatendo os radicais livres(causadores do envelhecimento cutâneo) e ativando a regeneração celular',
+      'Óleos vegetais são biodegradáveis, não poluem e nem agridem o meio ambiente',
+    ],
     stamps: { artesanal: 'Produto artesanal' },
   },
   {
@@ -11,7 +19,17 @@ export default [
     path: 'desodorante-pasta',
     ingredients:
       'BUTYROSPERMUM PARKII (Manteiga de Karite), HYDROGENATED PALM KERNEL GLYCERIDES (Óleo de Coco Palmiste), SODIUM BICARBONATE (Bicarbonato de Sódio), TAPIOCA STARCH (Fécula de Mandioca), TOCOPHERYL (Vitamina E), MELALEUCA ALTERNIFOLIA OIL (Óleo Essencial de Tea Tree), LAVANDULA ANGUSTIFOLIA OIL (Óleo Essencial de Lavanda).',
-    benefits: ['Não bloqueia os gânglios', 'Promove a limpeza de bactérias'],
+    benefits: [
+      'Protege por mais tempo e com mais intensidade',
+      'Indicado para maior nível de atividade',
+      'Manteiga karite, além de hidratar a pele da axila, funciona como fixador no desodorante',
+      'Por ser no pote, fica fácil de usar até a última "gota"',
+      'Protege do mau cheiro sem agredir sua saúde',
+      'Não entope os poros',
+      'Não impede seu suor',
+      'Não intoxica',
+      'Não polui o ambiente',
+    ],
     stamps: {
       artesanal: 'Produto artesanal',
       'todos-niveis': 'Bom para todos os níveis de atividade',
@@ -22,7 +40,17 @@ export default [
     path: 'desodorante-bisnaga',
     ingredients:
       'BUTYROSPERMUM PARKII (Manteiga de Karite), HYDROGENATED PALM KERNEL GLYCERIDES (Óleo de Coco Palmiste), SODIUM BICARBONATE (Bicarbonato de Sódio), TAPIOCA STARCH (Fécula de Mandioca), TOCOPHERYL (Vitamina E), MELALEUCA ALTERNIFOLIA OIL (Óleo Essencial de Tea Tree), LAVANDULA ANGUSTIFOLIA OIL (Óleo Essencial de Lavanda).',
-    benefits: ['Não bloqueia os gânglios', 'Promove a limpeza de bactérias'],
+    benefits: [
+      'Protege por mais tempo e com mais intensidade',
+      'Indicado para maior nível de atividade',
+      'Manteiga karite, além de hidratar a pele da axila, funciona como fixador no desodorante',
+      'Por ser no pote, fica fácil de usar até a última "gota"',
+      'Protege do mau cheiro sem agredir sua saúde',
+      'Não entope os poros',
+      'Não impede seu suor',
+      'Não intoxica',
+      'Não polui o ambiente',
+    ],
     stamps: {
       artesanal: 'Produto artesanal',
       'todos-niveis': 'Bom para todos os níveis de atividade',
@@ -33,7 +61,17 @@ export default [
     path: 'desodorante-rollon',
     ingredients:
       'WATER (Água purificada), MAGNESIUM HYDROXIDE (Hidróxido de Magnésio em suspenção), MELALEUCA ALTERNIFOLIA OIL (Óleo Essencial de Tea Tree), LAVANDULA ANGUSTIFOLIA OIL (Óleo Essencial de Lavanda).',
-    benefits: ['Não bloqueia os gânglios', 'Promove a limpeza de bactérias'],
+    benefits: [
+      'Desodorante suave',
+      'Indicado para peles sensíveis',
+      'Indicado para o dia - a - dia',
+      'Fácil aplicação',
+      'Protege do mau cheiro sem agredir sua saúde',
+      'Não entope os poros',
+      'Não impede seu suor',
+      'Não intoxica',
+      'Não polui o ambiente',
+    ],
     stamps: {
       artesanal: 'Produto artesanal',
       'todos-niveis': 'Bom para todos os níveis de atividade',
@@ -44,6 +82,11 @@ export default [
     path: 'rosa-mosqueta',
     ingredients:
       'ROSA AFF RUBIGINOSA OIL (Óleo de Rosa Mosqueta 100%vegetal e prensado a frio) e LAVANDULA ANGUSTIFOLIA OIL (Óleo Essencial de Lavanda), CYMBOPOGON MARTINI (Óleo Essencial de Palmarosa).',
+    benefits: [
+      'Óleo vegetal de Rosa Mosqueta é conhecido por sua ação regeneradora da pele',
+      'Ajuda a reduzir manchas, marcas, linhas de expressão',
+      'Hidrata e protege a pele',
+    ],
     stamps: { artesanal: 'Produto artesanal' },
   },
   {
@@ -51,19 +94,37 @@ export default [
     path: 'hidratante-facial',
     ingredients:
       'ALOE GEL (Aqua, Aloe vera gel, Acrylates/C10-30 alkyl acrylate crosspolymer, Aminomethyl propanol, Caprylhydroxamic acid, Glycerin, Caprylylgycol), ROSA AFF RUBIGINOSA OIL (óleo rosa mosqueta), HYDROGENATED PALM KERNEL GLYCERIDESL (Óleo coco palmiste), TOCOPHERYL ACETATE (Vitamina E), LAVANDULA ANGUSTIFOLIA OIL (Óleo essencial de lavanda), CYMBOPOGON MARTINI (Palmarosa), Copaíba, COFFEA ARABICA (Café verde).',
+    benefits: [
+      'Óleos essenciais são extraídos de plantas e altamente concentrados',
+      'Eles carregam o poder das plantas',
+      'Nosso hidratante tem óleos essenciais de Palmarosa, Lavanda, Copaíba e Café Verde',
+      'Pra todo esse poder chegar em vc através da sua pele',
+    ],
     stamps: { artesanal: 'Produto artesanal' },
   },
   {
     name: 'Hidratante Corporal',
     path: 'hidratante-corporal',
     ingredients:
-      'ALOE GEL (Aqua, Aloe vera gel, Acrylates/C10-30 alkyl acrylate crosspolymer, Aminomethyl propanol, Caprylhydroxamic acid, Glycerin, Caprylylgycol), ROSA AFF RUBIGINOSA OIL (óleo rosa mosqueta), HYDROGENATED PALM KERNEL GLYCERIDESL (Óleo coco palmiste), TOCOPHERYL ACETATE (Vitamina E), LAVANDULA ANGUSTIFOLIA OIL (Óleo essencial de lavanda), CYMBOPOGON MARTINI (Palmarosa), Copaíba, COFFEA ARABICA (Café verde).',
+      'ALOE GEL (Aqua, Aloe vera gel, Acrylates/C10-30 alkyl acrylate crosspolymer, Aminomethyl propanol, Caprylhydroxamic acid, Glycerin, Caprylylgycol), ROSA AFF RUBIGINOSA OIL (óleo rosa mosqueta), HYDROGENATED PALM KERNEL GLYCERIDESL (Óleo coco palmiste), TOCOPHERYL ACETATE (Vitamina E), LAVANDULA ANGUSTIFOLIA OIL (Óleo essencial de lavanda), CYMBOPOGON MARTINI(Palmarosa), Copaíba, COFFEA ARABICA (Café verde).',
+    benefits: [
+      'Não queremos fingir, queremos ser!',
+      'Óleos minerais e seus derivados não penetram na nossa pele, eles apenas criam uma película sobre ela',
+      'São chamados oclusivos e impedem nossa pele de respirar',
+      'Os óleos vegetais realmente penetram, hidratam e nutrem nossa pele',
+    ],
     stamps: { artesanal: 'Produto artesanal' },
   },
   {
     name: 'Pó Dental',
     path: 'po-dental',
     ingredients: 'MELALEUCA ALTERNIFOLIA OIL (Óleo Essencial de Tea Tree)',
+    benefits: [
+      'Mínimo para o máximo!',
+      'Mínimo de ingredientes e processamentos',
+      'Máximo em resultado',
+      'Sabe quando passa a lingua nos dentes e sente eles bem lisinhos? Sensação boa né ? Melhor ainda é saber que vc faz isso sem causar riscos para seus dentes, sua saúde e seu único planeta <3',
+    ],
     stamps: { artesanal: 'Produto artesanal' },
   },
 ]
